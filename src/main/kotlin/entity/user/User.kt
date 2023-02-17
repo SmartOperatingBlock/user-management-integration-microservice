a510a0f190f3e8eff148c886047fb6e00333ede2
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-17 14:49:23 +0100

    feat(entity): create user model

---
+++ src/main/kotlin/entity/user/User.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.user
+
+import entity.user.UserData.PASSWORD_MAX_LENGTH
+import entity.user.UserData.PASSWORD_MIN_LENGTH
+
+/** The user of the Smart Operating block System. */
+data class User(
+
+    /** The identifier of the [User]. */
+    val userId: String,
+
+    /** The [password] of the [User]. */
+    val password: String
+) {
+    init {
+        require(userId.isNotEmpty()) {
+            throw IllegalArgumentException("Invalid userId!")
+        }
+
+        require(password.length in PASSWORD_MIN_LENGTH..PASSWORD_MAX_LENGTH) {
+            throw IllegalArgumentException("Invalid user password!")
+        }
+    }
+}
---
+++ src/main/kotlin/entity/user/UserData.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.user
+
+/**
+ * Module with some user data.
+ */
+object UserData {
+
+    /** The minimum length of the password. */
+    const val PASSWORD_MIN_LENGTH = 8
+
+    /** The maximum length of the password. */
+    const val PASSWORD_MAX_LENGTH = 20
+}
