ba4472c26366b415bb8e393395e1dd8e9ce635d1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-17 14:49:47 +0100

    feat(entity): create health professional model

---
+++ src/main/kotlin/entity/healthprofessional/HealthProfessional.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.healthprofessional
+
+import entity.healthprofessional.HealthProfessionalData.Gender
+import entity.healthprofessional.HealthProfessionalData.HealthProfessionalRole
+import entity.healthprofessional.HealthProfessionalData.PhoneNumber
+
+/**
+ * The Health Professional interface.
+ */
+interface HealthProfessional {
+
+    /** The identifier of the [HealthProfessional]. */
+    val healthProfessionalId: String
+
+    /** The [name] of the [HealthProfessional]. */
+    val name: String
+
+    /** The [surname] of the [HealthProfessional]. */
+    val surname: String
+
+    /** The [gender] of the [HealthProfessional]. */
+    val gender: Gender
+
+    /** The [birthdate] of the [HealthProfessional]. */
+    val birthdate: String
+
+    /** The [emailAddress] of the [HealthProfessional]. */
+    val emailAddress: String
+
+    /** The [phoneNumber] of the [HealthProfessional]. */
+    val phoneNumber: PhoneNumber
+
+    /** The [role] of the [HealthProfessional]. */
+    val role: HealthProfessionalRole
+}
---
+++ src/main/kotlin/entity/healthprofessional/HealthProfessionalData.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.healthprofessional
+
+import java.util.*
+
+/**
+ * Module with all the Health Professionals data.
+ */
+object HealthProfessionalData {
+
+    /** The Surgeon health professional model. */
+    data class Surgeon(
+        override val healthProfessionalId: String,
+        override val name: String,
+        override val surname: String,
+        override val gender: Gender,
+        override val birthdate: String,
+        override val emailAddress: String,
+        override val phoneNumber: PhoneNumber,
+        override val role: HealthProfessionalRole = HealthProfessionalRole.SURGEON
+    ) : HealthProfessional {
+        init {
+            require(healthProfessionalId.isNotEmpty()) {
+                throw IllegalArgumentException("Invalid HealthProfessional ID!")
+            }
+        }
+    }
+
+    /** The Surgeon health professional model. */
+    data class Nurse(
+        override val healthProfessionalId: String,
+        override val name: String,
+        override val surname: String,
+        override val gender: Gender,
+        override val birthdate: String,
+        override val emailAddress: String,
+        override val phoneNumber: PhoneNumber,
+        override val role: HealthProfessionalRole = HealthProfessionalRole.NURSE
+    ) : HealthProfessional {
+        init {
+            require(healthProfessionalId.isNotEmpty()) {
+                throw IllegalArgumentException("Invalid HealthProfessional ID!")
+            }
+        }
+    }
+
+    /** The Surgeon health professional model. */
+    data class Anesthetist(
+        override val healthProfessionalId: String,
+        override val name: String,
+        override val surname: String,
+        override val gender: Gender,
+        override val birthdate: String,
+        override val emailAddress: String,
+        override val phoneNumber: PhoneNumber,
+        override val role: HealthProfessionalRole = HealthProfessionalRole.ANESTHETIST
+    ) : HealthProfessional {
+        init {
+            require(healthProfessionalId.isNotEmpty()) {
+                throw IllegalArgumentException("Invalid HealthProfessional ID!")
+            }
+        }
+    }
+
+    /** All types of Health professional gender. */
+    enum class Gender {
+        MALE, FEMALE, OTHER
+    }
+
+    /** All types of health professional role. */
+    enum class HealthProfessionalRole {
+        SURGEON, ANESTHETIST, NURSE
+    }
+
+    /** The phone [number] preceded by the [prefix]. */
+    data class PhoneNumber(
+        val prefix: String,
+        val number: String
+    )
+}
