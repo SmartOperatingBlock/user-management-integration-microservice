7430e47a0e5c6246ac67f5c5e77d336d567e0007
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-02-20 10:37:46 +0100

    feat: create user repository

---
+++ src/main/kotlin/usecase/repository/UserRepository.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase.repository
+
+import entity.user.User
+
+/**
+ * The repository for user management.
+ */
+interface UserRepository {
+
+    /** Create the given [user]. */
+    fun createUser(user: User): User?
+
+    /** Delete a user from its [userId]. */
+    fun deleteUser(userId: String): Boolean
+
+    /** Returns a [User] given its [userId]. */
+    fun getUser(userId: String): User
+}
